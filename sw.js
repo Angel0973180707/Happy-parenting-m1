382ff49d9be6d77d70950d92338938342cbe9701
Angel0973180707 <a0973180707@gmail.com>
2025-12-23 07:14:27 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,36 @@
+const CACHE = "HP_M1_V1_0_0"; // 以後更新就改版本字串
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
 
+self.addEventListener("install", (event)=>{
+  event.waitUntil(
+    caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event)=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.map(k=>k===CACHE?null:caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event)=>{
+  event.respondWith(
+    caches.match(event.request).then(cached=>{
+      if(cached) return cached;
+      return fetch(event.request).then(res=>{
+        const copy = res.clone();
+        caches.open(CACHE).then(c=>c.put(event.request, copy));
+        return res;
+      });
+    })
+  );
+});
